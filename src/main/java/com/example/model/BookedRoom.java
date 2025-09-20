c9d29ea2cc74114da1a83e5172c39359022a1afe
Ratcha000 <ratcha.k@kkumail.com>
2025-09-20 18:02:49 +0700

    write code .model
    write BookedRoom and Room and add package BookedRoomResponse and RoomResponse

--- src/main/java/com/example/model/BookedRoom.java
+++ src/main/java/com/example/model/BookedRoom.java
@@ -1,5 +1,91 @@
 package com.example.model;
 
-public class BookedRoom {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookedRoom {
+	
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long bookingId;
+	
+	@Column(name = "check_In")
+	private LocalDate checkInDate;
+	
+	@Column(name = "check_Out")
+	private LocalDate checkOutDate;
+	
+	@Column(name = "guest_FullName")
+	private String guestFullName;
+	
+	@Column(name = "guest_Email")
+	private String guestEmail;
+	
+	@Column(name = "adults")
+	private int NumOfAdults;
+	
+	@Column(name = "children")
+	private int NumOfChildren;
+	
+	@Column(name = "total_guest")
+	private int totalNumOfGuest;
+	
+	@Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+	
+	
+	public void calculateTotalNumberOfGuest() {
+		this.totalNumOfGuest = this.NumOfAdults + NumOfChildren;
+	}
+
+
+	public void setNumOfAdults(int numOfAdults) {
+		NumOfAdults = numOfAdults;
+		calculateTotalNumberOfGuest();
+	}
+
+
+	public void setNumOfChildren(int numOfChildren) {
+		NumOfChildren = numOfChildren;
+		calculateTotalNumberOfGuest();
+		
+	}
+
+
+	 public void setBookingConfirmationCode(String bookingConfirmationCode) {
+	        this.bookingConfirmationCode = bookingConfirmationCode;
+	    }
+
+	 public void setRoom(Room room) {
+	        this.room = room;
+	    }
+
+	
+	
+	
+	
 }
--- src/main/java/com/example/model/Room.java
+++ src/main/java/com/example/model/Room.java
@@ -1,5 +1,60 @@
 package com.example.model;
 
-public class Room {
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import com.example.model.BookedRoom;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roomType;
+	private BigDecimal roomPrice;
+	private boolean isBooked = false;
+	
+	@Lob
+	private Blob photo;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<BookedRoom> bookings;
+ 
+ 	public Room() {
+	 this.bookings = new ArrayList<>();
+ 	}
+ 	
+ 	public void addBooking(BookedRoom booking) {
+ 		if(bookings == null) {
+ 			bookings = new ArrayList<>();
+ 		}
+ 		 bookings.add(booking);
+         booking.setRoom(this); // <<< แก้ตรงนี้ ถูกต้อง
+         isBooked = true;
+         String bookingCode = RandomStringUtils.randomNumeric(10); // <<< แก้ syntax ตรงนี้
+         booking.setBookingConfirmationCode(bookingCode); 
+ 	}
+ 	
+ 
 }
